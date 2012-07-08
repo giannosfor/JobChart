c78db07e45476b5ec445a085131bf88e18f44b3b
Γιάννης Χριστοφάκης <giannis_christofakis@hotmail.com>
2012-07-08 15:42:55 +0300

    Update master

--- src/Extras/Fields.java
+++ src/Extras/Fields.java
@@ -1,4 +1,3 @@
-
 package Extras;
 
 public interface Fields {
@@ -6,6 +5,6 @@ public interface Fields {
   public static final String database = "jdbc:mysql://localhost:3306/jobfinder";
   public static final String driver = "com.mysql.jdbc.Driver";
   public static final String user = "giannis";
-  public static final String password = "giannis";
+  public static final String password = "****";
 
 }
